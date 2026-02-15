c6c4707eb196747b07ff05a359986afc99a98df7
ze-nien <bb0145786932@gmail.com>
2026-02-15 16:21:53 +0800

    fix: recipe copy button

--- src/Chef/Chef_SuggestRecipe.jsx
+++ src/Chef/Chef_SuggestRecipe.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactMarkDown from "react-markdown";
 
 const Chef_SuggestRecipe = ({ children }) => {
-  const copyHandler = (text) => {
-    navigator.clipboard.writeText(text);
+  const copyHandler = () => {
+    const cleanText = children
+      .replace(/[#*`~]/g, "")
+      .replace(/\[(.*?)\]\(.*?\)/g, "$1");
+    navigator.clipboard.writeText(cleanText);
     alert("copied");
   };
-
   return (
     <div
       style={{
@@ -16,31 +18,45 @@ const Chef_SuggestRecipe = ({ children }) => {
         fontSize: "1.2rem",
       }}
     >
-      <button
-        style={{ position: "absolute", bottom: "10px", right: "10px" }}
-        onClick={() => copyHandler(children)}
-      >
-        copy
-      </button>
-      <ReactMarkDown
-        components={{
-          ul: ({ children }) => (
-            <ul style={{ paddingLeft: "2rem", listStyleType: "disc" }}>
-              {children}
-            </ul>
-          ),
-          ol: ({ children }) => (
-            <ol style={{ paddingLeft: "2rem", listStyleType: "decimal" }}>
-              {children}
-            </ol>
-          ),
-          li: ({ children }) => (
-            <li style={{ marginBottom: "0.5rem" }}>{children}</li>
-          ),
-        }}
-      >
-        {children}
-      </ReactMarkDown>
+      {children && (
+        <div style={{ position: "absolute", right: "10px" }}>
+          <button
+            style={{
+              width: "3rem",
+              height: "1.5rem",
+              backgroundColor: "green",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+            }}
+            onClick={copyHandler}
+          >
+            copy
+          </button>
+        </div>
+      )}
+
+      <div>
+        <ReactMarkDown
+          components={{
+            ul: ({ children }) => (
+              <ul style={{ paddingLeft: "2rem", listStyleType: "disc" }}>
+                {children}
+              </ul>
+            ),
+            ol: ({ children }) => (
+              <ol style={{ paddingLeft: "2rem", listStyleType: "decimal" }}>
+                {children}
+              </ol>
+            ),
+            li: ({ children }) => (
+              <li style={{ marginBottom: "0.5rem" }}>{children}</li>
+            ),
+          }}
+        >
+          {children}
+        </ReactMarkDown>
+      </div>
     </div>
   );
 };
